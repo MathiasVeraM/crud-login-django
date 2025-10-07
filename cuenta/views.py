1c89f98b5954b3996c4f3e09fff1cd03cffa8dda
MathiasVeraM <mathias.vera@udla.edu.ec>
2025-10-06 19:31:03 -0500

    Se crearon las vistas y templates, login, logout, register funcionales

--- cuenta/views.py
+++ cuenta/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
+from django.db import IntegrityError
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
+def home(request):
+    return render(request, 'home.html')
+
+def signup(request):
+    if request.method == 'GET':
+            return render(request, 'signup.html', {
+            'form' : UserCreationForm
+            })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            # registrar usuario
+            try:
+                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+                user.save()
+                return redirect('signin')
+            except IntegrityError:
+                return render(request, 'signup.html', {
+                    'form' : UserCreationForm,
+                    'error' : "Usuario ya existe"
+                })
+    return render(request, 'signup.html', {
+                    'form' : UserCreationForm,
+                    'error' : "Contraseñas no coinciden"
+                })
+
+def signout(request):
+    logout(request)
+    return redirect('home')
+
+def signin(request):
+    if request.method == 'GET':
+        return render(request, 'signin.html', {
+            'form' : AuthenticationForm
+        })
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'signin.html', {
+                'form' : AuthenticationForm,
+                'error' : "Usuario o contraseña no valido"
+            })
+        else:
+            login(request, user)
+            return redirect('home')
+        
--- loginproject/settings.py
+++ loginproject/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'cuenta',
 ]
 
 MIDDLEWARE = [
@@ -115,6 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+LOGIN_URL = '/signin'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
